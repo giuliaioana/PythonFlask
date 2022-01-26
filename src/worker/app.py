b55efba40e34ad131b10d447cfe825af77fab73f
giuliaioana <62292005+giuliaioana@users.noreply.github.com>
2022-01-27 00:24:42 +0200

    Update app.py

--- src/worker/app.py
+++ src/worker/app.py
@@ -5,8 +5,8 @@ import json
 import pymysql.cursors
 import os
 
-hostname = "ip-172-31-6-119" if os.getenv("SWARM") else "mysql"
-rabitmq_host = "ip-172-31-6-119" if os.getenv("SWARM") else "rabbitmq"
+hostname = "34.247.39.37" if os.getenv("SWARM") else "mysql"
+rabitmq_host = "34.247.39.37" if os.getenv("SWARM") else "rabbitmq"
 
 def get_db_password() -> str:
     try:
